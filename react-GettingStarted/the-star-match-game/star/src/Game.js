738f0f61206138131b3b7a43a8cbb67979ea8893
mjoze <mirek.jozefiak@gmail.com>
2020-04-14 17:07:49 +0200

    using custom hooks

--- react-GettingStarted/the-star-match-game/star/src/Game.js
+++ react-GettingStarted/the-star-match-game/star/src/Game.js
@@ -5,7 +5,9 @@ import PlayAgain from './PlayAgain';
 import './Game.css';
 
 
-const Game = (props) => {
+// Custom Hook
+
+const useGameState = () => {
     const [stars, setStars] = useState(utils.random(1, 9));
     // const stars = 9;
     const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
@@ -19,7 +21,37 @@ const Game = (props) => {
             }, 1000);
             return () => clearTimeout(timerId);
         }
-    });
+    })
+    const setGameState = (newCandidateNums) => {
+        if (utils.sum(newCandidateNums) !== stars) {
+            setCandidateNums(newCandidateNums);
+        } else {
+            const newAvailableNums = availableNums.filter(n => !newCandidateNums.includes(n));
+            setStars(utils.randomSumIn(newAvailableNums, 9));
+            setAvailableNums(newAvailableNums);
+            setCandidateNums([]);
+        }
+    }
+    return { stars, availableNums, candidateNums, secondsLeft, setGameState };
+}
+
+const Game = (props) => {
+    // const [stars, setStars] = useState(utils.random(1, 9));
+    // // const stars = 9;
+    // const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+    // const [candidateNums, setCandidateNums] = useState([]);
+    // const [secondsLeft, setSecondsLeft] = useState(10);
+
+    // useEffect(() => {
+    //     if (secondsLeft > 0 && availableNums.length > 0) {
+    //         const timerId = setTimeout(() => {
+    //             setSecondsLeft(secondsLeft - 1)
+    //         }, 1000);
+    //         return () => clearTimeout(timerId);
+    //     }
+    // });
+
+    const { stars, availableNums, candidateNums, secondsLeft, setGameState } = useGameState();
 
     const candidatesAreWrong = utils.sum(candidateNums) > stars;
     // const gameIsDone = availableNums.length === 0;
@@ -49,14 +81,17 @@ const Game = (props) => {
 
         const newCandidateNums =
             currentStatus === 'available' ? candidateNums.concat(number) : candidateNums.filter(cn => cn !== number);
-        if (utils.sum(newCandidateNums) !== stars) {
-            setCandidateNums(newCandidateNums);
-        } else {
-            const newAvailableNums = availableNums.filter(n => !newCandidateNums.includes(n));
-            setStars(utils.randomSumIn(newAvailableNums, 9));
-            setAvailableNums(newAvailableNums);
-            setCandidateNums([]);
-        }
+
+        setGameState(newCandidateNums);
+
+        // if (utils.sum(newCandidateNums) !== stars) {
+        //     setCandidateNums(newCandidateNums);
+        // } else {
+        //     const newAvailableNums = availableNums.filter(n => !newCandidateNums.includes(n));
+        //     setStars(utils.randomSumIn(newAvailableNums, 9));
+        //     setAvailableNums(newAvailableNums);
+        //     setCandidateNums([]);
+        // }
     }
 
     return (
