56496b8f12b8f198304cd5f8f418bc2ec3865fb3
mjoze <mirek.jozefiak@gmail.com>
2020-04-21 14:37:03 +0200

    implement centrailized changes...

--- react-redux/src/components/courses/ManageCoursePage.js
+++ react-redux/src/components/courses/ManageCoursePage.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import * as authorActions from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-class ManageCoursesPage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+function ManageCoursesPage({
+  courses,
+  authors,
+  loadAuthors,
+  loadCourses,
+  ...props
+}) {
+  const [course, setCourse] = useState({ ...props.course });
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch((error) => {
         alert("Loading courses failed" + error);
@@ -17,17 +27,13 @@ class ManageCoursesPage extends React.Component {
         alert("Loading authors failed" + error);
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }, []);
+
+  return <CourseForm course={course} errors={errors} authors={authors} />;
 }
 
 ManageCoursesPage.propTypes = {
+  course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   loadCourses: PropTypes.func.isRequired,
@@ -36,6 +42,7 @@ ManageCoursesPage.propTypes = {
 
 function mapStateToProps(state) {
   return {
+    course: newCourse,
     courses: state.authors,
     authors: state.authors,
   };
