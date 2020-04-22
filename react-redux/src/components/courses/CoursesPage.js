b067fcc833dbe98f455f9f1b92b26b6222332648
mjoze <mirek.jozefiak@gmail.com>
2020-04-22 16:48:28 +0200

    Error handling

--- react-redux/src/components/courses/CoursesPage.js
+++ react-redux/src/components/courses/CoursesPage.js
@@ -6,6 +6,8 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import CourseList from "./CourseList";
 import { Redirect } from "react-router-dom";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 class CoursesPage extends React.Component {
   state = {
@@ -24,20 +26,36 @@ class CoursesPage extends React.Component {
       });
     }
   }
+
+  handleDeleteCourse = (course) => {
+    toast.success("Course deleted");
+    this.props.actions.deleteCourse(course).catch((error) => {
+      toast.error("Delete failed. " + error.message, { autoClose: false });
+    });
+  };
+
   render() {
     return (
       <>
         {this.state.redirectToAddCoursesPage && <Redirect to="/course/" />}
         <h2>Courses</h2>
-
-        <button
-          style={{ marginBottom: 20 }}
-          className="btn btn-primary add-course"
-          onClick={() => this.setState({ redirectToAddCoursesPage: true })}
-        >
-          Add Course
-        </button>
-        <CourseList courses={this.props.courses} />
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <>
+            <button
+              style={{ marginBottom: 20 }}
+              className="btn btn-primary add-course"
+              onClick={() => this.setState({ redirectToAddCoursesPage: true })}
+            >
+              Add Course
+            </button>
+            <CourseList
+              onDeleteClick={this.handleDeleteCourse}
+              courses={this.props.courses}
+            />
+          </>
+        )}
       </>
     );
   }
@@ -47,6 +65,7 @@ CoursesPage.propTypes = {
   authors: PropTypes.array.isRequired,
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state) {
@@ -62,6 +81,7 @@ function mapStateToProps(state) {
             };
           }),
     authors: state.authors,
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
@@ -70,6 +90,7 @@ function mapDispatchToProps(dispatch) {
     actions: {
       loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
       loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch),
+      deleteCourse: bindActionCreators(courseActions.deleteCourse, dispatch),
     },
   };
 }
